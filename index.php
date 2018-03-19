f1c83ef418c9fcdb2b3314b39547a87eaae0e2c1
Frontend Update <arte@update.pe>
2018-03-19 17:59:54 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	//titlesHeaderOrsite
+	include 'particles/head.php';
+?>
+<body style="background-color: grey;">
+
+
+
+
+
+
+
+
+<?php
+	include 'layout/menu.php';
+	include 'layout/slide.php';
+	include 'layout/info.php';
+	//callJ&Scritp
+	include 'particles/script.php';
+?>
+</body>
+</html>
---
+++ layout/info.php
@@ -0,0 +1,11 @@
+<div class="container-flui" style="background-color: #fffefe;">
+	<div class="row">
+		<div class="col-md-12" align="center">
+			<h3 class="ptsans">RECONOCIDOS</h3>
+			<h1 class="ptsans">CORREDORES DE SEGUROS</h1>
+			<h3 class="ptsans">EN LIMA - PERÚ</h3>
+			<p>Somos Lorem ipsum dolor, consectetur adipisicing elit, do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim , quis nostrud exercitation  laboris.</p>
+			
+		</div>
+	</div>
+</div>
